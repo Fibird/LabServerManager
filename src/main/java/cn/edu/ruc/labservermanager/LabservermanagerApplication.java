c08e4274da60b6526b5bd17add54436119019f18
Liu Chaoyang <chaoyanglius@gmail.com>
2021-02-28 09:41:57 +0800

    initial commit
    * init spring boot project
    * use spring boot dev, lombok and etc

---
+++ src/main/java/cn/edu/ruc/labservermanager/LabservermanagerApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.ruc.labservermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabservermanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabservermanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/ruc/labservermanager/LabservermanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.ruc.labservermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabservermanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
